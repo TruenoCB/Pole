d7efee815c00645ea0b8e9b08e7a92242ebf48b9
caibo <cb181018@163.com>
2023-04-16 21:24:18 +0800

    分组控制

--- pole-web/main.go
+++ pole-web/main.go
@@ -29,6 +29,29 @@ func main() {
 	r.GET("/assets/*filepath", func(c *poleweb.Context) {
 		c.JSON(http.StatusOK, poleweb.H{"filepath": c.Param("filepath")})
 	})
+	v1 := r.Group("/v1")
+	{
+		v1.GET("/", func(c *poleweb.Context) {
+			c.HTML(http.StatusOK, "<h1>Hello Gee</h1>")
+		})
+
+		v1.GET("/hello", func(c *poleweb.Context) {
+			c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
+		})
+	}
+	v2 := r.Group("/v2")
+	{
+		v2.GET("/hello/:name", func(c *poleweb.Context) {
+			c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
+		})
+		v2.POST("/login", func(c *poleweb.Context) {
+			c.JSON(http.StatusOK, poleweb.H{
+				"username": c.PostForm("username"),
+				"password": c.PostForm("password"),
+			})
+		})
+
+	}
 
 	r.Run(":1018")
 }
--- pole-web/poleweb/pole.go
+++ pole-web/poleweb/pole.go
@@ -6,34 +6,60 @@ import (
 )
 
 // HandlerFunc方法实现
-//type HandlerFunc func(http.ResponseWriter, *http.Request)
-//参数修改为context类型
+// type HandlerFunc func(http.ResponseWriter, *http.Request)
+// 参数修改为context类型
 type HandlerFunc func(*Context)
 
 // 实现ServeHTTP方法即实现了接口
 type Engine struct {
+	*RouterGroup
+	groups []*RouterGroup // store all groups
 	router *router
+}
+
+type RouterGroup struct {
+	prefix      string
+	middlewares []HandlerFunc // 中间件支持
+	parent      *RouterGroup  // support nesting
+	engine      *Engine       // all groups share a Engine instance
 }
 
 // 结构体的构造器
 func New() *Engine {
-	return &Engine{router: newRouter()}
+	engine := &Engine{router: newRouter()}
+	engine.RouterGroup = &RouterGroup{engine: engine}
+	engine.groups = []*RouterGroup{engine.RouterGroup}
+	return engine
+}
+
+// Group is defined to create a new RouterGroup
+// remember all groups share the same Engine instance
+func (group *RouterGroup) Group(prefix string) *RouterGroup {
+	engine := group.engine
+	newGroup := &RouterGroup{
+		prefix: group.prefix + prefix,
+		parent: group,
+		engine: engine,
+	}
+	engine.groups = append(engine.groups, newGroup)
+	return newGroup
 }
 
 // 添加方法到路由表中
-func (engine *Engine) addRoute(method string, pattern string, handler HandlerFunc) {
+func (group *RouterGroup) addRoute(method string, comp string, handler HandlerFunc) {
+	pattern := group.prefix + comp
 	log.Printf("Route %4s - %s", method, pattern)
-	engine.router.addRoute(method, pattern, handler)
+	group.engine.router.addRoute(method, pattern, handler)
 }
 
 // 定义GET请求的方法并添加到engine的HandlerFunc方法字典中
-func (engine *Engine) GET(pattern string, handler HandlerFunc) {
-	engine.addRoute("GET", pattern, handler)
+func (group *RouterGroup) GET(pattern string, handler HandlerFunc) {
+	group.addRoute("GET", pattern, handler)
 }
 
 // 定义POST请求的方法并添加到engine的HandlerFunc方法字典中
-func (engine *Engine) POST(pattern string, handler HandlerFunc) {
-	engine.addRoute("POST", pattern, handler)
+func (group *RouterGroup) POST(pattern string, handler HandlerFunc) {
+	group.addRoute("POST", pattern, handler)
 }
 
 // 开启一个http server
